a18c9c2f7d4a02bec5f4f232fab5907e63672cdf
baixoo <sofia.baixo27@gmail.com>
2026-02-13 17:55:48 +0000

    script de ingestão de data (draft) (mudança por causa de erros do linter)

--- scripts/ingest_data.py
+++ scripts/ingest_data.py
@@ -1,8 +1,9 @@
 import os
+
 import pandas as pd
-from sqlalchemy import create_engine
-from pymongo import MongoClient
 from dotenv import load_dotenv
+from pymongo import MongoClient
+from sqlalchemy import create_engine
 
 load_dotenv()
 
@@ -10,22 +11,17 @@ PG_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWO
 MONGO_URL = "mongodb://localhost:27017/"
 
 def ingest_structured_data(file_path):
-    """Insere dados estruturados no PostgreSQL"""
     engine = create_engine(PG_URL)
     df = pd.read_csv(file_path)
-    
     df.to_sql('saude_transacional', engine, if_exists='append', index=False, chunksize=10000)
     print(f"Inseridos {len(df)} registos no PostgreSQL.")
 
 def ingest_unstructured_data(json_data):
-    """Insere documentos ou logs no MongoDB"""
     client = MongoClient(MONGO_URL)
     db = client['helth_db']
     collection = db['logs_saude']
-    
     collection.insert_many(json_data)
     print(f"Inseridos {len(json_data)} documentos no MongoDB.")
 
 if __name__ == "__main__":
-    #usar funções para os dados
     pass
